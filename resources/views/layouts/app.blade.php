80334bf49259f13a60febae28731dfa43ca2e9fd
getmaxed <maxfaden07@gmail.com>
2018-02-22 01:00:05 -0800

    Created methods to edit and update blog posts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,8 +18,9 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand" href="{{ url('/blog') }}">
+                    newStuff
+                    {{--{{ config('app.name', 'newStuff') }}--}}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -28,7 +29,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        {{--<li><a class="nav-link" href="{{ url('blog') }}">Blog</a></li>--}}
                     </ul>
 
                     <!-- Right Side Of Navbar -->
